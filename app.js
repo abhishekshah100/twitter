7777a3acd7c60e15560cc26df72503f12ff33768
abhishekshah100 <abhishekshah100@gmail.com>
2021-03-08 23:27:54 +0530

    Mddleware Completed

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware');
+const loginRoute = require('./routes/loginRoutes');
 
 const server = app.listen(port, ()=> console.log(`Server Listening the Port is ${port}`) );
 
 app.set('view engine','pug');
 app.set('views','views');
+
+app.use("/login",loginRoute);
 
 app.get('/',middleware.requireLogin,(req, res, next)=>{
     const payload = {
