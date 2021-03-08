d2279bb64a3d326d8a04f8676b0aa9e18a8de0a0
abhishekshah100 <abhishekshah100@gmail.com>
2021-03-09 02:10:15 +0530

    Register For Design Ready

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 const port = 3003;
 const middleware = require('./middleware');
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const server = app.listen(port, ()=> console.log(`Server Listening the Port is ${port}`) );
 
 app.set('view engine','pug');
 app.set('views','views');
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname,"public")));
+
 app.use("/login",loginRoute);
+app.use("/register",registerRoute);
 
 app.get('/',middleware.requireLogin,(req, res, next)=>{
     const payload = {
