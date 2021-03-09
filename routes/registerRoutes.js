d08d61dfd4868e84e50cd3fa6f9e42c9f100013e
abhishekshah100 <abhishekshah100@gmail.com>
2021-03-09 22:15:35 +0530

    Inserting user into collection

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+let User = require('../schema/UserSchema');
 
 app.set('view engine','pug');
 app.set('views','views');
@@ -12,17 +13,48 @@ router.get('/', (req, res, next)=>{
     res.status(200).render('register');
 });
 
-router.post('/', (req,res, next)=> {
-    console.log(req.body);
+router.post('/', async (req,res, next)=> {
+    //console.log(req.body);
     let firstName=req.body.firstName.trim();
     let lastName=req.body.lastName.trim();
     let userName=req.body.userName.trim();
     let email=req.body.email.trim();
     let password=req.body.password;
     var payload = req.body;
-    if(firstName && lastName && userName && password)
+    if(firstName && lastName && userName && email && password)
     {
-        console.log('success');
+        var user = await User.findOne({
+            $or : [
+                { userName: userName},
+                { email : email }
+            ]
+        })
+        .catch((error)=> {
+            console.log(error);
+            payload.errorMessage = "Something went wrong";
+            res.status(200).render("register",payload);
+        });
+        if(user==null)
+        {
+            //No User found
+            var data = req.body;
+            User.create(data)
+            .then((user) => {
+                console.log("create");
+            })
+        }
+        else
+        {
+            //User found
+            if(email==user.email)
+            {
+                payload.errorMessage = "Email Already Exists";
+            }
+            else{
+                payload.errorMessage = "UserName Already Exists";
+            }
+            res.status(200).render("register",payload);
+        }
     }
     else
     {
