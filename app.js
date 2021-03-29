94172383143327f3c0c3a28cf9b90ac223c2e0a7
abhishekshah100 <abhishekshah100@gmail.com>
2021-03-29 16:32:16 +0530

    Retweet Completed

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3003;
-const middleware = require('./middleware');
-const loginRoute = require('./routes/loginRoutes');
-const registerRoute = require('./routes/registerRoutes');
-const path = require('path');
-const mongoose = require('./database');
-const bodyParser = require('body-parser');
+const port = 1003;
+const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser")
+const mongoose = require("./database");
 const session = require("express-session");
 
-const server = app.listen(port, ()=> console.log(`Server Listening the Port is ${port}`) );
+const server = app.listen(port, () => console.log("Server listening on port " + port));
 
-app.set('view engine','pug');
-app.set('views','views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
     secret: "bbq chips",
@@ -23,14 +21,25 @@ app.use(session({
     saveUninitialized: false
 }))
 
-app.use("/login",loginRoute);
-app.use("/register",registerRoute);
+// Routes
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
-app.get("/",middleware.requireLogin, (req, res, next)=>{
+// Api routes
+const postsApiRoute = require('./routes/api/posts');
+
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+
+app.use("/api/posts", postsApiRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+
     var payload = {
-        pageTitle: 'Home Page Working',
-        userLoggedIn: req.session.user
+        pageTitle: "Home",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user),
     }
-    //res.status(200).send('Yahoo!!!');
-    res.status(200).render('home',payload);
-});
+
+    res.status(200).render("home", payload);
+})
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,61 +1,52 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-let User = require('../schema/UserSchema');
-const { post } = require('./registerRoutes');
+const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt");
+const User = require('../schemas/UserSchema');
 
-app.set('view engine','pug');
-app.set('views','views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/', (req, res, next)=>{
-    res.status(200).render('login');
-});
+router.get("/", (req, res, next) => {
+    
+    res.status(200).render("login");
+})
 
-router.post('/', async(req, res, next) => {
-    let userName=req.body.loginUsername;
-    let password=req.body.logPassword;
+router.post("/", async (req, res, next) => {
+
     var payload = req.body;
-    if(userName && password)
-    {
+
+    if(req.body.logUsername && req.body.logPassword) {
         var user = await User.findOne({
-            $or : [
-                { userName: userName},
-                { email : userName }
+            $or: [
+                { username: req.body.logUsername },
+                { email: req.body.logUsername }
             ]
         })
-        .catch((error)=> {
+        .catch((error) => {
             console.log(error);
-            payload.errorMessage = "Something went wrong";
-            res.status(200).render("login",payload);
+            payload.errorMessage = "Something went wrong.";
+            res.status(200).render("login", payload);
         });
-        if(user!=null)
-        {
-            //User found
-            let result =await bcrypt.compare(password, user.password);
-            if(result===true)
-            {
-                req.session.user=user;
+        
+        if(user != null) {
+            var result = await bcrypt.compare(req.body.logPassword, user.password);
+
+            if(result === true) {
+                req.session.user = user;
                 return res.redirect("/");
             }
-            else
-            {
-                payload.errorMessage = "Password is incorrect";
-            }
         }
-        else
-        {
-            payload.errorMessage = "Email or UserName Does not exist Exists";
-        }
+
+        payload.errorMessage = "Login credentials incorrect.";
+        return res.status(200).render("login", payload);
     }
-    else
-    {
-        payload.errorMessage = "Make sure each field has a valid value.";
-    }
-    return res.status(200).render("login",payload);
+
+    payload.errorMessage = "Make sure each field has a valid value.";
+    res.status(200).render("login");
 })
 
 module.exports = router;
